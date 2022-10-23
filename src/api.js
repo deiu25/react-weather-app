1cc0c4f6717e2070d30a29526bbf8fbe8b60e1a6
deiu25 <d3iu25@gmail.com>
2022-10-23 19:52:55 +0300

    second commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "417cc05f66mshfa0ee5f1cee76c8p1edf04jsn3c405648de02",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
