edbe48cf7442f8b46870a5ea1a7df40c2fcaa917
deiu25 <d3iu25@gmail.com>
2022-10-24 20:11:12 +0300

    Fetching and mapping data from weather API

--- src/api.js
+++ src/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "a80f3905442df3a5e1a9724b947f688a";
