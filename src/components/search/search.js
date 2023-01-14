6dbf47fc8412b4a0179aa891e4a34f493b5bf1c1
deiu25 <d3iu25@gmail.com>
2023-01-14 12:30:50 +0200

    some style add

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,3 +1,4 @@
+import "./search.css";
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { GEO_API_URL, geoApiOptions } from "../../api";
@@ -6,7 +7,7 @@ const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
@@ -25,16 +26,20 @@ const Search = ({ onSearchChange }) => {
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
+    setSearch(null);
   };
 
   return (
-    <AsyncPaginate
-      placeholder="Search for city"
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={loadOptions}
-    />
+    <div className="search-container">
+      <AsyncPaginate
+        className="search-input"
+        placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+      />
+    </div>
   );
 };
 export default Search;
